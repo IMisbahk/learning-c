b1c284d9202264475d9ff1aed07ca32d085cff5e
Misbah Khursheed <m.misbahkhursheed@gmail.com>
2026-03-10 20:41:18 +0530

    basics:helloword - new line char

--- programs/basics/helloWorld.c
+++ programs/basics/helloWorld.c
@@ -2,5 +2,11 @@
 
 
 int main() {
+    // printf("testing without newline char");
     printf("hello, world\n");
+ 
 }
+
+// the slash-n is a newline character, it moves cursor to next line after printing
+
+
